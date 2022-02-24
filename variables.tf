dbcb4c126a4361bfbb930420a118952569eb9804
Sergei Ugdyzhekov <sergei.ugdyzhekov@jetbrains.com>
2022-02-24 13:27:41 +0100

    feat: Tags refactoring and typo fixes

--- variables.tf
+++ variables.tf
@@ -1,23 +1,24 @@
-variable "tags" {
-  description = "Tags."
-  type        = map(string)
-}
 variable "name" {
-  description = "Prefix of your network name, 'RDS' or 'App' e.g."
+  description = "Network name, 'RDS' or 'App' e.g."
 }
+
 variable "zone" {
   default = "eu-west-1a"
 }
+
 variable "type" {
   description = "Network type: 'private' or 'public'"
   default     = "private"
 }
+
 variable "subnet_cidr" {
   description = "Subnet CIDR."
 }
+
 variable "route_table" {
   description = "Route table id."
 }
+
 data "aws_route_table" "current" {
   route_table_id = local.route_table
 }
@@ -29,10 +30,11 @@ locals {
   name        = var.name
   type        = lower(var.type)
   az          = var.zone
-  tags = merge({
+
+  tags = {
     Name         = local.name
     Type         = local.type
     Module       = "Subnet"
     ModuleSource = "https://github.com/jetbrains-infra/terraform-aws-routed-subnet/"
-  }, var.tags)
+  }
 }
